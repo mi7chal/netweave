0b9af74ed962f25c06b2199e8044d5965ae26590
Michal Chruscielski <michalch775@gmail.com>
2026-03-07 22:08:53 +0100

    feat: database auto creation from docker image

--- migrations/005_users_and_icons.sql
+++
@@ -1,2 +0,0 @@
--- Add icon_url to services table
-ALTER TABLE services ADD COLUMN IF NOT EXISTS icon_url VARCHAR(255);
--- migrations/006_oidc_auto_import_default.sql
+++
@@ -1,4 +0,0 @@
--- Default: OIDC auto import is opt-in.
-INSERT INTO app_settings (key, value)
-VALUES ('oidc_auto_import', 'false')
-ON CONFLICT (key) DO NOTHING;
