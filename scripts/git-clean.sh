453ea7c2a38c0ee30c9dfe7d38676d104d26957c
Michał Chruścielski <46084566+mi7chal@users.noreply.github.com>
2026-03-13 00:37:39 +0100

    Chore/scripts improvement (#12)
    * chore: git cleanup script
    
    * chore: release-plz config
    
    * chore: release-plz github action
    
    * chore: release ci compatibility
    
    * chore: docker-publish.yml cleanup and update
    
    * chore: pre-commit hooks with prek
    
    * chore: todo check

---
+++ scripts/git-clean.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# git-clean.sh - removes local branches that have been removed from the remote repository
+# Usage: ./scripts/git-clean.sh
+
+echo "Checking for removed branches.."
+
+# Sync
+if ! git fetch -p; then
+    echo "Error: 'git fetch' failed."
+    exit 1
+fi
+
+# Standard git command to find branches that have been removed from the remote repository
+# LIST=$(git branch -vv | awk '/: gone]/ {print $1}')
+LIST=$(git branch -vv | grep ': gone]' | awk '{print $1}')
+
+if [ -z "$LIST" ]; then
+    echo "Everything is up to date!"
+    exit 0
+fi
+
+ERRORS=$(echo "$LIST" | xargs git branch -d 2>&1)
+
+if [ $? -ne 0 ]; then
+    echo "Warning: Some branches couldn't be deleted (probably unmerged changes)."
+    echo "$ERRORS" | grep -E "error|detail" || echo "$ERRORS"
+else
+    COUNT=$(echo "$LIST" | wc -l | tr -d ' ')
+    echo "Successfully removed $COUNT branches!"
+fi
